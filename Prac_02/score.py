51ff3e043ddd201bd329f461c1ceaef1b0292b7e
Ryan <ryan.bout@my.jcu.edu.au>
2022-09-25 21:30:06 +1000

    Add functions & random score

--- Prac_02/score.py
+++ Prac_02/score.py
@@ -2,19 +2,28 @@
 CP1404practicals/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 
 def main():
     """Print result from user score"""
     score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+    random_score = random.randint(1, 100)
+    result = determine_result(random_score)
+    print(f"Score: {random_score} - {result}")
+
+
+def determine_result(score):
+    """Return result from user score"""
     if score < 0 or score > 100:
-        print("Invalid score")
-    elif score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    else:
-        print("Bad")
+        return "Invalid score"
+    if score >= 90:
+        return "Excellent"
+    if score >= 50:
+        return "Passable"
+    return "Bad"
 
 
 main()
