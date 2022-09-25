b1be1260015dc23f2bbe6276b81a24cc7414007f
Ryan <ryan.bout@my.jcu.edu.au>
2022-09-25 21:22:40 +1000

    Fix boundary conditions

--- Prac_02/score.py
+++ Prac_02/score.py
@@ -3,26 +3,18 @@ CP1404practicals/CP5632 - Practical
 Broken program to determine score status
 """
 
-# Using boundaries instead of checking in range
 
-score = float(input("Enter score: "))
-while 0 <= score <= 100:
-    if score > 90:
+def main():
+    """Print result from user score"""
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
         print("Excellent")
-    elif score > 50:
+    elif score >= 50:
         print("Passable")
     else:
         print("Bad")
-    score = float(input("Enter score: "))
 
-# Using range for boundaries
 
-# score = float(input("Enter score: "))
-# while score in range(101):
-#     if score > 90:
-#         print("Excellent")
-#     elif score > 50:
-#         print("Passable")
-#     else:
-#         print("Bad")
-#     score = float(input("Enter score: "))
+main()
