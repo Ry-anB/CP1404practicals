b73e0de343f9a484c84774ed1fc4227871bd09ad
Ryan <ryan.bout@my.jcu.edu.au>
2022-10-12 17:42:54 +1000

    Edit function for returning data and format output

--- Prac_4/subject_reader.py
+++ Prac_4/subject_reader.py
@@ -7,23 +7,23 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    """Print data from lists to show teacher & number of students for subjects"""
+    datasets = get_data()
+    for data in datasets:
+        print(f"{data[0]} is taught by {data[1]} and has {data[2]} students")
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    datasets = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        datasets.append(parts)
     input_file.close()
+    return datasets
 
 
 main()
