2143b68f452a2727ffa4f528f2aa48862e76324f
Ryan <ryan.bout@my.jcu.edu.au>
2022-11-10 19:54:57 +1000

    Fix prac_07 branch

---
+++ prac_07/project.py
@@ -0,0 +1,41 @@
+"""CP1404/CP5632 Practical 07 - Project Class"""
+
+from datetime import datetime
+
+
+class Project:
+    """Stores project details in class."""
+
+    def __init__(self, name="", start_date=0, priority=0, estimate=0, completion=0):
+        """Initialise a project."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.estimate = estimate
+        self.completion = completion
+
+    def __str__(self):
+        """Return details of a project"""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}," \
+               f" estimate: ${self.estimate}, completion: {self.completion}%"
+
+    def is_complete(self):
+        """Determine if a project is complete."""
+        return self.completion >= 100
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __ge__(self, other):
+        return self.start_date >= other.start_date
+
+    def __lt__(self, other):
+        return self.start_date < other.start_date
+
+    def update_completion(self, completion):
+        """Update completion for project"""
+        self.completion = completion
+
+    def update_priority(self, priority):
+        """Update priority for project"""
+        self.priority = priority
